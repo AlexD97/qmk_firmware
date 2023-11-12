2bfc27870468d92a7e9ee68329374f5d897ab7bc
Alexander Dinges <alexding97@gmail.com>
2023-11-12 12:15:22 +0100

    Alex keyboard

--- keyboards/handwired/dactyl_manuform/6x7_qwerty/config.h
+++ keyboards/handwired/dactyl_manuform/6x7_qwerty/config.h
@@ -30,9 +30,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // wiring of each half
 //#define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B5, B7 }
 //#define MATRIX_ROW_PINS { F5, F6, F7, B1, B3, B2, B6 }
-//#define MATRIX_COL_PINS { D1, D4, C6, D7, E6, B4, B5 } // right half 
+#define MATRIX_COL_PINS { D1, D4, C6, D7, E6, B4, B5 } // right half 
 #define MATRIX_ROW_PINS { F5, F6, F7, B1, B3, B2, B6 }
-#define MATRIX_COL_PINS { B5, B4, E6, D7, C6, D4, D1 } //left half
+//#define MATRIX_COL_PINS { B5, B4, E6, D7, C6, D4, D1 } //left half
 
 //#define DIODE_DIRECTION COL2ROW
 #define DIODE_DIRECTION ROW2COL
